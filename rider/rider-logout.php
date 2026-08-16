358dc03e3c092471097efd90b95a995b3750386b
Zohaib Khan <zohaibkaki987@gmail.com>
2026-08-16 15:15:57 +0500

    Add secure rider logout handler

---
+++ rider/rider-logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
+session_destroy();
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Location: rider-login.php?logout=1');
+exit;
+?>
