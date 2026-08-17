3b82a73fc83e30f709323d9cdd706c507e994186
Zohaib Khan <zohaibkaki987@gmail.com>
2026-08-17 15:54:43 +0500

    Fix application timezone for rider session booking

--- includes/session.php
+++ includes/session.php
@@ -1,4 +1,8 @@
 <?php
+
+/* Humsafar operates on Pakistan Standard Time (UTC+05:00). */
+date_default_timezone_set('Asia/Karachi');
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
